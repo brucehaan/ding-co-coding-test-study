a99864bee60c58565728ead627623894939ba3ee
Bruce Han <intragerhan@gmail.com>
2026-08-04 19:53:59 +0900

    곱하기 or 더하기 풀이

---
+++ src/week1/Main3.java
@@ -0,0 +1,24 @@
+package week1;
+
+public class Main3 {
+
+    public static int findMaxPlusOrMultiply(int[] array) {
+        int answer = 0;
+        for (int i : array) {
+            if (answer == 0) answer += i;
+            else if (i == 0 || i == 1) answer += i;
+            else answer *= i;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {0, 3, 5, 6, 1, 2, 4};
+        int[] arr2 = {3, 2, 1, 5, 9, 7, 4};
+        int[] arr3 = {1, 1, 1, 3, 3, 2, 5};
+
+        System.out.println("정답 = 728 현재 풀이 값 = " + findMaxPlusOrMultiply(arr1));
+        System.out.println("정답 = 8820 현재 풀이 값 = " + findMaxPlusOrMultiply(arr2));
+        System.out.println("정답 = 270 현재 풀이 값 = " + findMaxPlusOrMultiply(arr3));
+    }
+}
