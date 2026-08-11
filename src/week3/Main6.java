14e7b04598c055cd43aacdc83759429c6d6837af
Bruce Han <intragerhan@gmail.com>
2026-08-11 20:02:16 +0900

    스택

---
+++ src/week3/Main6.java
@@ -0,0 +1,71 @@
+package week3;
+
+public class Main6 {
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class Stack {
+        private Node head;
+
+        public Stack() {
+            this.head = null;
+        }
+
+        public void push(int value) {
+            Node newHead = new Node(value);
+            newHead.next = this.head;
+            this.head = newHead;
+        }
+
+        public Node pop() {
+            if (isEmpty()) {
+                System.out.println("stack is Empty");
+                return null;
+            }
+
+            Node deleteHead = this.head;
+            this.head = this.head.next;
+            return deleteHead;
+        }
+
+        public Object peek() {
+            if (isEmpty()) {
+                return "stack is Empty";
+            }
+
+            return this.head.data;
+        }
+
+        public boolean isEmpty() {
+            return this.head == null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(4);
+        System.out.println(stack.peek());
+
+        stack.push(3);
+        System.out.println(stack.peek());
+
+        stack.push(5);
+        System.out.println(stack.peek());
+
+        stack.pop();
+        System.out.println(stack.peek());
+
+        stack.pop();
+        System.out.println(stack.peek());
+
+        stack.pop();
+        System.out.println(stack.peek());
+    }
+}
